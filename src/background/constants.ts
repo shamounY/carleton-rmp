8c13f0edcd964cd3c27c7f9d4783fa6113dc104e
shamounY <shamoun.yousuf04@gmail.com>
2025-06-11 00:40:46 -0400

    move constants to seperate file

---
+++ src/background/constants.ts
@@ -0,0 +1,9 @@
+export const SCHOOL_IDS = [
+    "U2Nob29sLTE0MjA=", // Carleton ID
+    "U2Nob29sLTE0NTI=", // UOttawa ID
+    "U2Nob29sLTE1MjA="  // Algonquin ID
+];
+
+export const URL = 'https://www.ratemyprofessors.com/graphql';
+
+export const AUTH_TOKEN = 'dGVzdDp0ZXN0';
